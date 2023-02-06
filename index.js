eb8ef6993ad7bc5e77f7d46f5e63243f4157bb37
torobucci <kevintoro254@gmail.com>
2023-02-06 15:03:02 +0300

    Fix linter errors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const faBars = document.querySelector('.fa-bars');
 const main = document.querySelector('.main');
 const menuToggle = document.querySelector('.menu-toggle');
-const menuToggleAnchor = document.querySelector('.toggle-ul')
+const menuToggleAnchor = document.querySelector('.toggle-ul');
 
-faBars.addEventListener("click", function () {
-    faBars.classList.toggle('fa-xmark')
-    main.classList.toggle('none')
-    menuToggle.classList.toggle('menu-toggle-height')
-})
+faBars.addEventListener('click', () => {
+  faBars.classList.toggle('fa-xmark');
+  main.classList.toggle('none');
+  menuToggle.classList.toggle('menu-toggle-height');
+});
 
-menuToggleAnchor.addEventListener("click", function () {
-    faBars.classList.toggle('fa-xmark')
-    main.classList.toggle('none')
-    menuToggle.classList.toggle('menu-toggle-height')
-})
+menuToggleAnchor.addEventListener('click', () => {
+  faBars.classList.toggle('fa-xmark');
+  main.classList.toggle('none');
+  menuToggle.classList.toggle('menu-toggle-height');
+});
